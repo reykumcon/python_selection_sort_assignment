f680e194082625288245ed4335f255ed309e8a3e
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-03-13 12:35:12 -1000

    Added a counter for each iteration

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,7 @@
 def selection_sort(arr):
 
+    count = 0
+    
     for j in range(len(arr)-1):
         
         min = j
@@ -9,7 +11,8 @@ def selection_sort(arr):
                 min = i
             
         arr[j], arr[min] = arr[min], arr[j]
+        count += 1
 
-    print(arr)
+        print("Iteration", count, "-",arr)
 
 selection_sort([10,8,6,4,2,0,9,7,5,3,1])
